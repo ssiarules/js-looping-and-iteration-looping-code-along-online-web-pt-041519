4d63dba85ea3d044b80c872be5832a270ae1f9d4
Johnson Joseph <jhonsonj88@gmail.com>
2020-01-04 01:53:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+  for (let i = 0; i < names.length; i++){
+  names[i] = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+}
+  return names;
+}
+
+function countdown(number){
+  while ( number > -1 ){
+    console.log(number);
+      number--;
+  }
+}
